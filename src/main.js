2e4a95d49caa8a7230bdc908d2b248a4dd3e44fb
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-09-01 21:50:05 +0300

    Add and configure vue-router

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import "bootstrap"
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routes/routes'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/routes/routes.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const SignUp = () => import('../views/SignUp.vue');
+
+const routes = [
+    {
+        path: "/signup",
+        name: "SignUp",
+        component: SignUp
+    },   
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router;
