43ea949c69a68a8128e2a93c5a5f901274e988cc
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-09-03 22:21:23 +0300

    Remove sign out button from sidebar and add it on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,24 @@
 <template>
-    <div class="row border border-danger w-100 navbarHeight">
-            <nav class="navbar bg-body-tertiary">
-                    <div class="container-fluid">
-                        <a class="navbar-brand text-dark fs-3 fw-bold" href="#">Mbegera Health Care</a>
-                    </div>
+    <div class="w-100 navbarHeight">
+            <nav class="navbar bg-body-tertiary w-100 px-4">
+                <div class="">
+                    <a class="navbar-brand text-dark fs-3 fw-bold" href="#">Mbegera Health Care</a>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-outline-secondary" @click="signOut">Sign Out</button>
+                </div>
             </nav>
   </div>
 </template>
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
+
+const signOut = () => {
+    localStorage.removeItem('SignedInToken'),
+    router.push('/signup')
+}
 
 </script>
 
@@ -15,6 +26,9 @@
 
 .navbarHeight {
     min-height: 80px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 }
 
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col col-2 border border-primary sidebar pt-5">
+    <div class="sidebar pt-5">
       <ul class="nav d-flex flex-column">  
         <li class="nav-item text-light">
           <router-link to="/" class="nav-link text-light fs-5 fw-semibold">
@@ -11,24 +11,11 @@
             Appointments
           </router-link>
         </li>
-        <li class="nav-item text-light">
-          <a class="nav-link text-light fs-5 fw-semibold" @click="signOut">
-            Sign Out
-          </a>
-        </li>
       </ul>
     </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
-
-const router = useRouter();
-
-const signOut = () => {
-    localStorage.removeItem('SignedInToken'),
-    router.push('/signup')
-}
 </script>
 
 <style scoped>
