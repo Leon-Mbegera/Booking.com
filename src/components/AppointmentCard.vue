0339fc16e96e7fe084e05407817d63d9686d42c4
Leon-Mbegera <mbegeraleon89@gmail.com>
2023-09-03 15:14:34 +0300

    Render appointment cards for each service

---
+++ src/components/AppointmentCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card mt-2 col ps-2">
+        <div class="card-header">
+            <p class="text-dark fs-4 fw-bold">{{ props.service.type }}</p>
+            <p class="badge rounded-pill text-bg-danger">Book now!</p>
+        </div>
+        <div class="card-body">
+            <img :src="props.service.image" class="card-img-top" alt="consultations.jpg">
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-danger w-100 fw-semibold">{{ props.service.buttonTitle }}</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(["service"]);
+
+</script>
--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,14 +1,51 @@
 <template>
     <DefaultLayoutVue>
         <div class="d-flex justify-content-between">
-            <Consultations />
-            <Checkup />
+            <div v-for="service in services" :key="service.type">
+                <AppointmentCard :service="service" />
+            </div>
+            <Teleport to="body">
+            <!-- use the modal component, pass in the prop -->
+            <modal :show="showModal" @close="showModal = false" @submit="onSubmit">
+              <template #header>
+                <h3>custom header</h3>
+              </template>
+            </modal>
+          </Teleport>
+          <button id="show-modal" @click="showModal = true">Show Modal</button>
         </div>
     </DefaultLayoutVue>
+
+
+
 </template>
 
 <script setup>
-import Consultations from '../components/Consultations.vue';
-import Checkup from '../components/checkups.vue';
+import AppointmentCard from '../components/AppointmentCard.vue';
 import DefaultLayoutVue from '../components/layouts/DefaultLayout.vue';
+import Modal from '../components/Modal.vue';
+import consultationImg from '../assets/consultations.jpg'
+import checkUpImg from '../assets/checkups.jpg'
+
+
+import { ref } from 'vue';
+
+const onSubmit = () => {
+  alert("ok");
+};
+
+const showModal = ref(false);
+
+const services = [
+    {
+        image: consultationImg,
+        type: "Consultations",
+        buttonTitle: "Book a Consultation",
+    },
+    {
+        image: checkUpImg,
+        type: "Check Ups",
+        buttonTitle: "Book a Check Up",
+    }
+]
 </script>
